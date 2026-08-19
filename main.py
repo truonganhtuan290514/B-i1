812583907ef0a718941c9c6028c90c7988b6c67f
truonganhtuan290514 <truonganhtuan290514@gmail.com>
2026-08-19 20:06:56 +0700

    Them kiem tra tuoi

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ print("Xin chao", name)
 age = int(input("Nhap tuoi cua ban: "))
 print("Ten:", name)
 print("Tuoi:", age)
+
+if age >= 18:
+    print("Ban da du 18 tuoi.")
+else:
+    print("Ban chua du 18 tuoi.")
